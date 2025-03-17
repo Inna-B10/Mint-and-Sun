fc911a65344f10a921570addeea67022b452c42c
Inna Aleksenitser <148863587+Inna-B10@users.noreply.github.com>
2025-03-17 14:46:15 +0100

    frontend: added animation yellow blocks

--- app/components/ScreenEgg/index.jsx
+++ app/components/ScreenEgg/index.jsx
@@ -1,13 +1,26 @@
+'use client'
 import cn from 'clsx'
+import { useEffect, useState } from 'react'
 import styles from './index.module.scss'
 
 export default function ScreenEgg({ className, children, type }) {
+	const [mounted, setMounted] = useState(false)
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setMounted(true)
+		}, 10)
+
+		return () => clearTimeout(timer)
+	}, [])
+
 	return (
 		<div
 			className={cn(
 				className,
 				styles.screenEgg,
-				type === 'right' ? styles.screenEggRight : styles.screenEggLeft
+				type === 'right' ? styles.screenEggRight : styles.screenEggLeft,
+				mounted ? styles.mounted : styles.preMount
 			)}>
 			{children}
 		</div>
